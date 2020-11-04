cc5d01dda0bce547bbf982ef4292c2cb76aee2ea
无肉不欢 <8271736+wurangkun520@user.noreply.gitee.com>
2020-11-04 23:53:23 +0800

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <router-view class="main" />
+  </div>
+</template>
+
+<style>
+@import 'assets/style/index.scss';
+
+#app{
+  width: 100%;
+}
+.main{
+  overflow: hidden;
+  width: 100%;
+}
+</style>
